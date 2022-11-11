19edd3d13075b679f443cbc6441a50b7fc9548bc
oussama <oussama@gmail.com>
2022-11-11 19:12:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("lbsInput");
+const output = document.getElementById("output");
+const gramsOutput = document.getElementById("gramsOutput");
+const kgOutput = document.getElementById("kgOutput");
+const ozOutput = document.getElementById("ozOutput");
+
+//eventlistener
+
+input.addEventListener("keyup", inputPounds);
+input.addEventListener("change", inputPounds);
+
+//functions
+
+function inputPounds() {
+  output.classList.remove("hidden");
+  gramsOutput.innerHTML = input.value * 453.592;
+  kgOutput.innerHTML = input.value * 0.453592;
+  ozOutput.innerHTML = input.value * 16;
+}
